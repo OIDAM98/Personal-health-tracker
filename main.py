bd523778210264f34779c31bf4541bf05a9de1cb
Oscar De Alva <oscar.ivan98@gmail.com>
2023-04-16 12:30:51 -0600

    Break into modules and save jsons locally Squashed commit of the following:
    commit 6cdf4e78775e07ac5a635391bc3076ba22f17294
    Author: Oscar De Alva <oscar.ivan98@gmail.com>
    Date:   Sun Apr 16 12:27:28 2023 -0600
    
        Save json data locally
    
    commit 12302f05657f87728a6363d66443ee1a2ad1e2c1
    Author: Oscar De Alva <oscar.ivan98@gmail.com>
    Date:   Sun Apr 16 12:12:38 2023 -0600
    
        Broke code into own module
    
    commit 82f00f0d7223625e491d6c2a9a4e83cc398910ed
    Author: Oscar De Alva <oscar.ivan98@gmail.com>
    Date:   Sun Apr 16 11:54:01 2023 -0600
    
        Broke into different functions for ETL
    
    commit 71470df89fbcbd33e7dbb2b8e272d32e939924e1
    Author: Oscar De Alva <oscar.ivan98@gmail.com>
    Date:   Sun Apr 16 11:22:06 2023 -0600
    
        Requirements.txt

--- main.py
+++ main.py
@@ -1,120 +1,16 @@
 import datetime
-import json
 import logging
-import sys
 import os
 from dotenv import load_dotenv
-
-import requests
-
-from garminconnect import (
-    Garmin,
-    GarminConnectAuthenticationError,
-    GarminConnectConnectionError,
-    GarminConnectTooManyRequestsError
+from tasks.consume import (
+    init_api
+    , get_overview_stats
+    , get_daily_activities
+    , transform_overview_stats
+    , transform_daily_activities
+    , prepare_json
+    , save_json
 )
-
-
-def format_json(data: dict, lvl:int=2) -> dict:
-    return json.dumps(data, indent=lvl)
-
-def init_api(email,password):
-    try:
-        with open('cached_session.json') as f:
-            saved =  json.load(f)
-
-            logger.info('Login to Garmin Connect using cached session...')
-
-            api = Garmin(session_data=saved)
-            api.login()
-    except (FileNotFoundError, GarminConnectAuthenticationError):
-        logger.info(
-            'No saved credentials.',
-            'Session cookies will be sotred in cached_session.json'
-        )
-
-        try:
-            api = Garmin(email, password)
-            api.login()
-
-            with open('cached_session.json', 'w', encoding='utf-8') as f:
-                json.dump(api.session_data, f, ensure_ascii=False, indent=2)
-
-        except(
-            GarminConnectConnectionError,
-            GarminConnectAuthenticationError,
-            GarminConnectTooManyRequestsError,
-            requests.exceptions.HTTPError
-        ) as err:
-            logger.error('Error ocurred during Garmin Connect comm %s', err)
-            return None
-
-    return api
-
-def get_today_stats(api):
-    wanted_stats = [ 'totalKilocalories'
-            ,'activeKilocalories'
-            ,'totalDistanceMeters'
-            ,'dailyStepGoal'
-            ,'activeSeconds'
-            ,'sedentarySeconds'
-            ,'sleepingSeconds'
-            ,'floorsAscendedInMeters'
-            ,'floorsDescendedInMeters'
-            ,'intensityMinutesGoal'
-            ,'minHeartRate'
-            ,'maxHeartRate'
-            ,'restingHeartRate'
-            ,'maxStressLevel'
-            ,'averageStressLevel'
-            ,'totalSteps'
-            ,'sleepingSeconds'
-            ,'stressPercentage'
-            ,'bodyBatteryChargedValue'
-            ,'bodyBatteryDrainedValue'
-            ,'bodyBatteryHighestValue'
-            ,'bodyBatteryLowestValue'
-            ,'stressQualifier'
-        ]
-    today_stats = api.get_stats(today.isoformat())
-    stats = {stat: today_stats[stat] for stat in wanted_stats}
-    stats = format_json(stats)
-    logger.info('Overview Data')
-    logger.info(stats)
-    return stats
-
-def get_daily_activities(api):
-    wanted_stats = [
-        'activityName'
-        ,'sportTypeId'
-        ,'startTimeLocal'
-        ,'activityType'
-        ,'distance'
-        ,'duration'
-        ,'elapsedDuration'
-        ,'movingDuration'
-        ,'averageSpeed'
-        ,'maxSpeed'
-        ,'calories'
-        ,'bmrCalories'
-        ,'averageHR'
-        ,'maxHR'
-        ,'steps'
-        ,'strokes'
-        ,'waterEstimated'
-        ,'averageRunningCadenceInStepsPerMinute'
-        ,'maxRunningCadenceInStepsPerMinute'
-        ,'averageSwimCadenceInStrokesPerMinute'
-        ,'maxSwimCadenceInStrokesPerMinute'
-    ]
-    activities = api.get_activities_by_date(today.isoformat(), today.isoformat())
-    activities_stats = { 'activities':
-                         [{stat: activity[stat] for stat in wanted_stats} for activity in activities]
-                        }
-    stats = format_json(activities_stats)
-    logger.info('Activities data')
-    logger.info(stats)
-    return stats
 
 if __name__=='__main__':
     FORMAT = "[%(asctime)s - %(levelname)s - %(funcName)s] %(message)s"
@@ -122,17 +18,18 @@ if __name__=='__main__':
     logging.basicConfig(level=logging.INFO, format=FORMAT, datefmt=DATEFORMAT)
     logger = logging.getLogger(__name__)
 
-
     load_dotenv()
 
     email = os.getenv('EMAIL')
     password = os.getenv('PASSWORD')
     api = None
 
-    today = datetime.date.today()
-    logger.info(f"Getting data for: {today.isoformat()}")
+    date_query = datetime.date.today()
+    logger.info(f"Getting data for: {date_query.isoformat()}")
     yesterday = datetime.date.today() - datetime.timedelta(days=1)
 
     api = init_api(email, password)
-    get_today_stats(api)
-    get_daily_activities(api)
+    overview = transform_overview_stats(get_overview_stats(api, date_query))
+    activities = transform_daily_activities(get_daily_activities(api, date_query))
+    data = prepare_json(overview, activities)
+    save_json(data, date_query)
---
+++ tasks/consume.py
@@ -0,0 +1,129 @@
+import datetime
+import json
+import logging
+import requests
+from garminconnect import (
+    Garmin,
+    GarminConnectAuthenticationError,
+    GarminConnectConnectionError,
+    GarminConnectTooManyRequestsError
+)
+
+logger = logging.getLogger(__name__)
+
+def format_json(data: dict, lvl:int=2) -> dict:
+    return json.dumps(data, indent=lvl)
+
+def init_api(email: str,password: str) -> Garmin:
+    try:
+        with open('cached_session.json') as f:
+            saved =  json.load(f)
+
+            logger.info('Login to Garmin Connect using cached session...')
+
+            api = Garmin(session_data=saved)
+            api.login()
+    except (FileNotFoundError, GarminConnectAuthenticationError):
+        logger.info(
+            'No saved credentials.',
+            'Session cookies will be sotred in cached_session.json'
+        )
+
+        try:
+            api = Garmin(email, password)
+            api.login()
+
+            with open('cached_session.json', 'w', encoding='utf-8') as f:
+                json.dump(api.session_data, f, ensure_ascii=False, indent=2)
+
+        except(
+            GarminConnectConnectionError,
+            GarminConnectAuthenticationError,
+            GarminConnectTooManyRequestsError,
+            requests.exceptions.HTTPError
+        ) as err:
+            logger.error('Error ocurred during Garmin Connect comm %s', err)
+            return None
+
+    return api
+
+def get_overview_stats(api: Garmin, date: datetime.datetime) -> dict:
+    stats = api.get_stats(date.isoformat())
+    logger.info(format_json(stats))
+    return stats
+
+def get_daily_activities(api: Garmin, date:datetime.datetime) -> dict:
+    activities = api.get_activities_by_date(date.isoformat(), date.isoformat())
+    logger.info(format_json(activities))
+    return activities
+
+def transform_overview_stats(data: dict) -> dict:
+    wanted_stats = [ 'totalKilocalories'
+            ,'activeKilocalories'
+            ,'totalDistanceMeters'
+            ,'dailyStepGoal'
+            ,'activeSeconds'
+            ,'sedentarySeconds'
+            ,'sleepingSeconds'
+            ,'floorsAscendedInMeters'
+            ,'floorsDescendedInMeters'
+            ,'intensityMinutesGoal'
+            ,'minHeartRate'
+            ,'maxHeartRate'
+            ,'restingHeartRate'
+            ,'maxStressLevel'
+            ,'averageStressLevel'
+            ,'totalSteps'
+            ,'sleepingSeconds'
+            ,'stressPercentage'
+            ,'bodyBatteryChargedValue'
+            ,'bodyBatteryDrainedValue'
+            ,'bodyBatteryHighestValue'
+            ,'bodyBatteryLowestValue'
+            ,'stressQualifier'
+        ]
+    stats = {stat: data[stat] for stat in wanted_stats}
+    logger.info(format_json(stats))
+    return stats
+
+def transform_daily_activities(data: dict) -> dict:
+    wanted_stats = [
+        'activityName'
+        ,'sportTypeId'
+        ,'startTimeLocal'
+        ,'activityType'
+        ,'distance'
+        ,'duration'
+        ,'elapsedDuration'
+        ,'movingDuration'
+        ,'averageSpeed'
+        ,'maxSpeed'
+        ,'calories'
+        ,'bmrCalories'
+        ,'averageHR'
+        ,'maxHR'
+        ,'steps'
+        ,'strokes'
+        ,'waterEstimated'
+        ,'averageRunningCadenceInStepsPerMinute'
+        ,'maxRunningCadenceInStepsPerMinute'
+        ,'averageSwimCadenceInStrokesPerMinute'
+        ,'maxSwimCadenceInStrokesPerMinute'
+    ]
+    stats = { 'activities':
+                         [{stat: activity[stat] for stat in wanted_stats} for activity in data]
+                        }
+
+    logger.info(format_json(stats))
+    return stats
+
+def prepare_json(data1:dict, data2:dict) -> dict:
+    concat = dict() | data1 | data2
+    logger.info(format_json(concat))
+    return concat
+
+def save_json(data: dict, date: datetime.datetime) -> None:
+    filename = f"{date.isoformat()}_data.json"
+    with open(filename, 'w') as f:
+        json.dump(data, f, separators=(',',':'))
+    logger.info(f"Saved data to {filename}")
